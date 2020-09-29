9788a5d482eed15f57b546d6fed7bece1b7c95dd
Kinkaid011 <k.nolen46@gmail.com>
2020-09-29 16:22:06 -0400

    added a color scheme

--- pages/index.js
+++ pages/index.js
@@ -12,14 +12,16 @@ export default function Home() {
       </Head>
 
       <div className="section">
-        <p>Hi, my name is</p>
-        <h3>Kinkaid Nolen</h3>
-        <h3 className={styles.colorGray}>a UI/UX Designer</h3>
+        <span>Hi, my name is</span>
+        <div className="name">
+          <h1>Kinkaid Nolen</h1>
+          <h1 className={styles.colorGray}>a UI/UX Designer</h1>
+        </div>
         <p>I design exceptional websites as well as applications. I specialize in UI/UX Design, Responsive Web Design, and Frontend Development.</p>
       </div>
 
       <div className="section">
-        <h1>Projects</h1>
+        <h1 className="title">Projects</h1>
         <div className="circle"></div>
 
         <div className={styles.sectionWrapper}>
@@ -35,30 +37,30 @@ export default function Home() {
       </div>
 
       <div className="section">
-        <h1>Resume</h1>
+        <h1 className="title">Resume</h1>
         <div className="circle"></div>
 
         <div className={styles.resumeWrapper}>
           <div className="projectBorder">
             <div className="projectWrapper">
-              <h4>Clients</h4> 
-              <h5>Strut Boutique</h5>
+              <h2 id={styles.marginBottom}>Clients</h2> 
+              <h4>Strut Boutique</h4>
               <p>A women's clothing boutique in Detriot Michigan</p>
             </div>
             <div className="projectWrapper">
-              <h5>MindBodyPilates</h5>
+              <h4>MindBodyPilates</h4>
               <p>A pilates studio in Massachusetts. There are also yoga classes as well.</p>
             </div>
             <div className="projectWrapper">
-              <h5>Phii for Hair</h5>
+              <h4>Phii for Hair</h4>
               <p>A Barbershop in Memphis Tennessee, where you get the best cuts</p>
             </div>
           </div>
 
           <div id={styles.grid} className="projectBorder">
             <div>
-              <h4>Skills</h4> 
-              <h5>Design</h5>
+              <h2 id={styles.marginBottom}>Skills</h2> 
+              <h4>Design</h4>
               <p>UI/UX Design <br/>
                 Wireframing <br/>
                 User Flows <br/>
@@ -67,8 +69,8 @@ export default function Home() {
                 Google Analytics <br/>
                 </p>
             </div>
-            <div id={styles.item} >
-              <h5>Technologies</h5>
+            <div id={styles.secondItem} >
+              <h4>Technologies</h4>
               <p>HTML5<br/>
                 CSS/SASS<br/>
                 Bootstrap<br/>
@@ -78,7 +80,7 @@ export default function Home() {
                 </p>
             </div>
             <div>
-              <h5>Tools</h5>
+              <h4>Tools</h4>
               <p>Figma<br/>
                 Adobe XD<br/>
                 Adobe Photoshop<br/>
@@ -91,12 +93,12 @@ export default function Home() {
       </div>
 
       <div className="section">
-        <h1>About Me</h1>
+        <h1 className="title">About Me</h1>
         <div className="circle"></div>
         <div className="aboutWrapper">
-          <h5>Who Am I?</h5>
+          <h2 id={styles.openSans}>Who Am I?</h2>
           <p>My name is KInkaid Nolenk, and I am a UI/UX Designer and Frontend Developer from Memphis, Tennessee. I have a B.S. Biology. Design and Code are my two forms of art.</p>
-          <h5>Why I do what I do?</h5>
+          <h2 id={styles.openSans}>Why I do what I do?</h2>
           <p>I have a true passion for web design and development. Learning to code has been a major improvement in my Life. Being able to create something that’s so ingrained in our culture is beyond rewarding. It’s an enriching experience to write clean code, and be apart of solutions that turn goals into reality.</p>
         </div>
       </div>
@@ -105,7 +107,7 @@ export default function Home() {
         <h1>Contact</h1>
         <div className="circle"></div>
         <div className="aboutWrapper">
-          <h5>Get in Touch</h5>
+          <h2 id={styles.openSans}>Get in Touch</h2>
           <p>Being able to create something that’s so ingrained in our culture is beyond rewarding. It’s an enriching experience to write clean code, and be apart of solutions that turn goals into reality.</p>
           <button>CONTACT ME</button>
         </div>
